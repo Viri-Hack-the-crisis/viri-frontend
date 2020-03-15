5a38bb78667e44441fd73797febfd6d8582fcd39
Punane <kadikerner@gmail.com>
2020-03-15 15:20:53 +0200

    profile fix

--- src/components/Profile/Profile.styles.tsx
+++ src/components/Profile/Profile.styles.tsx
@@ -1,7 +1,94 @@
 import styled from "styled-components";
 
 export const ProfileContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-content: left;
+    margin: auto;
+    max-width: 710px;
+    height: auto;
+    color: #346EA6;
+`;
+
+export const ReportContainer = styled.div`
+    justify-content: left;
+    margin: auto;
+    max-width: 710px;
+    height: auto;
+`;
+
+export const H1 = styled.div`
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
+    text-transform: uppercase;
+    color: #346EA6;
+    padding-bottom: 30px;
+    margin-top: 100px;
+`;
+
+export const H2 = styled.div`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 23px;
+    color: #346EA6;
+    padding-bottom: 26px;
+`;
+
+export const ProfileText = styled.p`
+    font-size: 16px;
+    line-height: 19px;
+    color: #346EA6;
+`;
+
+export const ProfileInfo = styled.div`
+    display: flex;
+    width: 715px;
+`;
+
+export const ProfileTopic = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+`;
+
+export const ProfileFackts = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+    justify-items: right;
+    width: 50%;
+`;
+
+export const ProfileButtonGreen = styled.button`
+    display: flex;
+    width: 50%;
+    justify-content: center;
+    border: 1px solid #1DE1CE;
+    box-sizing: border-box;
+    border-radius: 20px;
+    padding: 5px 33px;
+    color: #FFFCF9;
+    font-size: 16px;
+    line-height: 19px;
+    margin: 5px;
+    background-color: #1DE1CE;
+    margin-left: 200px;
+`;
+
+export const ProfileButtonYellow = styled.button`
+    display: flex;
+    width: 50%;
+    justify-content: right;
+    border: 1px solid #F8DB57;
+    box-sizing: border-box;
+    border-radius: 20px;
+    padding: 5px 33px;
+    color: #FFFCF9;
+    font-size: 16px;
+    line-height: 19px;
+    margin: 5px;
+    background-color: #F8DB57;
+    margin-left: 200px;
 `;
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -4,6 +4,9 @@ import { useHistory } from "react-router-dom";
 import { StoreContext } from "../../state/store";
 
 import { ProfileContainer } from "../../components/Profile/Profile.styles";
+import { H1 } from "../../components/Profile/Profile.styles";
+import { H2 } from "../../components/Profile/Profile.styles";
+import { ProfileText, ProfileInfo, ProfileTopic, ProfileFackts, ProfileButtonYellow, ProfileButtonGreen } from "../../components/Profile/Profile.styles";
 
 interface Props {}
 
@@ -19,7 +22,44 @@ const ProfilePage = (props: Props) => {
 
   return (
     <ProfileContainer>
-      Here
+      <div>
+        <div>
+          <H1>Your Profile</H1>
+          <ProfileInfo>
+            <ProfileTopic>
+              <ProfileText>Name</ProfileText>
+              <ProfileText>Gender</ProfileText>
+              <ProfileText>Date of Birth</ProfileText>
+              <ProfileText>Country of Residency</ProfileText>
+              <ProfileText>Place of Birth</ProfileText>
+            </ProfileTopic>
+            <ProfileFackts>
+              <ProfileText>Klaabu Gonzales</ProfileText>
+              <ProfileText>Male</ProfileText>
+              <ProfileText>08/02/1982</ProfileText>
+              <ProfileText>Estonia</ProfileText>
+              <ProfileText>United States of Grigoristan</ProfileText>
+            </ProfileFackts>
+          </ProfileInfo>
+        </div>
+        <div>
+          <H1>Your Self-checks</H1>
+          <ProfileInfo>
+            <ProfileTopic>
+              <H2>Date</H2>
+              <ProfileText>12.03.2020</ProfileText>
+              <ProfileText>09.03.2020</ProfileText>
+              <ProfileText>02.03.2020</ProfileText>
+            </ProfileTopic>
+            <ProfileFackts>
+              <H2>Result</H2>
+              <ProfileButtonGreen>High Risk </ProfileButtonGreen>
+              <ProfileButtonYellow>Moderate Risk</ProfileButtonYellow>
+              <ProfileButtonYellow>Moderate Risk</ProfileButtonYellow>
+            </ProfileFackts>
+          </ProfileInfo>
+        </div>
+      </div>
     </ProfileContainer>
   );
 };
